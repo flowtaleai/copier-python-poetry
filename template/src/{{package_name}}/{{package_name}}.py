d3ad454095480819093acb822155feadca5e0199
Rasmus Jones <rj@flowtale.ai>
2024-01-10 13:50:56 +0000

    fix type annotations

--- template/src/{{package_name}}/{{package_name}}.py
+++ template/src/{{package_name}}/{{package_name}}.py
@@ -4,6 +4,6 @@ from {{package_name}} import logs
 logger = logs.get_logger(__name__)
 
 
-def a_function():
+def a_function() -> str:
     logger.debug("Generating hello world string")
     return "Hello World!"
