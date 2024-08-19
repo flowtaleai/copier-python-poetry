af4202e4197f86176ed5b8023bcd694124422171
Franz Maria Heuchel <62-fhchl@users.noreply.gitlab.flowtale.ai>
2024-08-19 16:34:11 +0200

    generate mkdocs

---
+++ template/{% if generate_docs %}docs{% endif %}/scripts/readme_as_index.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+import shutil
+import mkdocs_gen_files
+
+root = Path(__file__).parent.parent.parent
+
+if not Path(root / "index.md").exists():
+    with mkdocs_gen_files.open("index.md", "w") as index:
+        with open(root / "README.md") as readme:
+            index.writelines(readme.readlines())
+        
+    
+
+
