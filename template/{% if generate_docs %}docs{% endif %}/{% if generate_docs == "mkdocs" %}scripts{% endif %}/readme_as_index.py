4fd7b430ae51ea1048eb9136287617258bf1ccc6
Franz Maria Heuchel <62-fhchl@users.noreply.gitlab.flowtale.ai>
2024-09-20 14:36:18 +0200

    add small fix for mkdocs script

--- template/{% if generate_docs %}docs{% endif %}/{% if generate_docs == "mkdocs" %}scripts{% endif %}/readme_as_index.py
+++ template/{% if generate_docs %}docs{% endif %}/{% if generate_docs == "mkdocs" %}scripts{% endif %}/readme_as_index.py
@@ -1,10 +1,11 @@
+"""Use the root README as index in case there is no docs/index.md yet."""
 from pathlib import Path
 
 import mkdocs_gen_files
 
 root = Path(__file__).parent.parent.parent
 
-if not Path(root / "index.md").exists():
+if not Path(root, "docs", "index.md").exists():
     with mkdocs_gen_files.open("index.md", "w") as index:
         with open(root / "README.md") as readme:
             index.writelines(readme.readlines())
